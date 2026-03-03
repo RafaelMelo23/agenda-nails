0d01a9595474e5bed05999d4d26f70dd448c659c
rafadev <eooraff@gmail.com>
2026-03-03 11:47:54 -0300

    sentry fix for catching async ex

--- src/main/java/com/rafael/nailspro/webapp/infrastructure/config/AsyncConfig.java
+++ src/main/java/com/rafael/nailspro/webapp/infrastructure/config/AsyncConfig.java
@@ -1,15 +1,28 @@
 package com.rafael.nailspro.webapp.infrastructure.config;
 
+import io.sentry.Sentry;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.Executor;
 
+@Slf4j
 @EnableAsync
 @Configuration
-public class AsyncConfig {
+public class AsyncConfig implements AsyncConfigurer {
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return (throwable, method, obj) -> {
+            log.error("Exception in @Async method: {}", method.getName(), throwable);
+            Sentry.captureException(throwable);
+        };
+    }
 
     @Bean(name = "messagingExecutor")
     public Executor messagingExecutor() {
